eafb47fe2c75d70f1998d85eca1e70c103dc4e0c
たいくん <98800458+taikun114@users.noreply.github.com>
2025-07-24 18:39:01 +0900

    Initial Commit

---
+++ Mocolamma/MocolammaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MocolammaApp.swift
+//  Mocolamma
+//
+//  Created by 今浦大雅 on 2025/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MocolammaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MocolammaTests/MocolammaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MocolammaTests.swift
+//  MocolammaTests
+//
+//  Created by 今浦大雅 on 2025/07/24.
+//
+
+import Testing
+@testable import Mocolamma
+
+struct MocolammaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
