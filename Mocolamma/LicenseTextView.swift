4a78386809b60e33c493fc6eec7e50054389e35f
たいくん <98800458+taikun114@users.noreply.github.com>
2025-08-07 18:48:38 +0900

    Make VisualEffectView independent

--- Mocolamma/LicenseTextView.swift
+++ Mocolamma/LicenseTextView.swift
@@ -1,23 +1,5 @@
 import SwiftUI
 
-// VisualEffectViewの定義を追加
-struct VisualEffectView: NSViewRepresentable {
-    var material: NSVisualEffectView.Material
-    var blendingMode: NSVisualEffectView.BlendingMode
-
-    func makeNSView(context: Context) -> NSVisualEffectView {
-        let view = NSVisualEffectView()
-        view.material = material
-        view.blendingMode = blendingMode
-        view.state = .active
-        return view
-    }
-
-    func updateNSView(_ nsView: NSVisualEffectView, context: Context) {
-        nsView.material = material
-        nsView.blendingMode = blendingMode
-    }
-}
 
 struct LicenseTextView: View {
     let licenseText: String
