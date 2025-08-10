20bfd945c2e714671fce33acee81272dcc26c739
たいくん <98800458+taikun114@users.noreply.github.com>
2025-08-10 17:24:06 +0900

    Improve iOS / iPadOS version to close the keyboard after swiping down the input field

--- Mocolamma/ChatInputView.swift
+++ Mocolamma/ChatInputView.swift
@@ -9,6 +9,20 @@ struct ChatInputView: View {
 
     var body: some View {
         MessageInputView(inputText: $inputText, isStreaming: $isStreaming, selectedModel: selectedModel, sendMessage: sendMessage, stopMessage: stopMessage)
-        
+        #if os(iOS)
+            .gesture(
+                DragGesture().onChanged { value in
+                    if value.translation.height > 30 { // 30ポイント下にドラッグ
+                        self.hideKeyboard()
+                    }
+                }
+            )
+        #endif
     }
+
+    #if os(iOS)
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    #endif
 }
